e3a2b6cc17329ca59848bc8af7168b40e8b16f53
dragoon4890 <harshsks123@gmail.com>
2023-07-29 01:49:43 +0530

    fixed error related to key iterator

--- src/components/bottom bar/bottombar.jsx
+++ src/components/bottom bar/bottombar.jsx
@@ -17,10 +17,10 @@ const Bottombar =()=>{
 
     const [active,setActive]= useState(0);
     const Menus = [
-        { name: "/", icon: homeOutline,  },
-        { name: "/location", icon: locationOutline, },
-        { name: "/ticket", icon: ticketOutline,  },
-        { name: "/user", icon: personOutline,  }
+        { name: "/", icon: homeOutline, key:0 },
+        { name: "/location", icon: locationOutline,key:1 },
+        { name: "/ticket", icon: ticketOutline, key:2 },
+        { name: "/user", icon: personOutline, key:3 }
         
       ];
     
@@ -30,11 +30,11 @@ const Bottombar =()=>{
     return (<div className={styles.panel}>
        
         <div className={styles.container}>
-            
+    
         {Menus.map((menu, i) => (
-         <div key={i} className={styles.botom}>
-             <span className={`${styles.box} ${i === active ? styles[`acti${i}`] : ''}`}></span>
-                     <Link href={menu.name} className={`${styles.links} ${i === active ? styles.active : ''}`} onClick={() => setActive(i)}>
+         <div key={i+30} className={styles.botom}>
+             <span key={i+10} className={`${styles.box} ${i === active ? styles[`acti${i}`] : ''}`}></span>
+                     <Link key={i+30} href={menu.name} className={`${styles.links} ${i === active ? styles.active : ''}`} onClick={() => setActive(i)}>
                           <IonIcon icon={menu.icon} className={styles.img} />
                     </Link>
         </div>
